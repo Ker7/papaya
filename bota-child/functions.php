5c2f264d27acb52292b544cd37d681472e502cd1
Kert G <kert.mottus@gmail.com>
2015-05-13 18:04:57 +0300

    banner wip

--- bota-child/functions.php
+++ bota-child/functions.php
@@ -5,3 +5,15 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+
+register_sidebar( array(
+		'name' => __( 'HeaderWiz', 'bota-child' ),
+		'id' => 'sidebar-head',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h1 class="widget-title">',
+		'after_title' => '</h1>',
+	) );
+
+?>
